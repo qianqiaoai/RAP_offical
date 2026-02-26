65af550ffeb8d2bba9a3876e00b334843bc589f5
joeqian <joeqian>
2026-02-26 06:11:09 +0000

    www

--- vite.config.js
+++ vite.config.js
@@ -23,7 +23,7 @@ export default defineConfig({
       ],
     }),
   ],
-  base:'/RAP',
+  base:'/RAP_offical',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
